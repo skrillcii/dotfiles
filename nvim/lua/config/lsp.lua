2966622d02d48606c13179cb05c8627a60b2d22e
skrillcii <skrillcii@github.com>
2025-12-14 23:09:52 +0900

    update

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -5,24 +5,24 @@
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("pyright")
 
--- Set up an LspAttach autocommand to enable features based on client
---  capabilities. A single autocommand can work for multiple LSP servers!
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
-
--- Add noselect to completeopt, otherwise autocompletion is annoying
-vim.cmd("set completeopt+=noselect")
-
 -- Enable rounded borders in floating windows
 vim.o.winborder = "rounded"
 
+-- Set up an LspAttach autocommand to enable features based on client
+--  capabilities. A single autocommand can work for multiple LSP servers!
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   callback = function(ev)
+--     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+--     if client:supports_method("textDocument/completion") then
+--       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+--     end
+--   end,
+-- })
+
+-- Add noselect to completeopt, otherwise autocompletion is annoying
+-- vim.cmd("set completeopt+=noselect")
+
 -- Enable virtual lines
-vim.diagnostic.config({
-  virtual_lines = true,
-})
+-- vim.diagnostic.config({
+--   virtual_lines = true,
+-- })
