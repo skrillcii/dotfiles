c21f2bdead75aab3c7799406a03de4ef54ebfab9
shu.morikuni <shu.morikuni@github.com>
2025-12-19 15:10:16 +0900

    update basedpyright

--- nvim/after/lsp/basedpyright.lua
+++ nvim/after/lsp/basedpyright.lua
@@ -1,1 +1,24 @@
-return {}
+return {
+  cmd = { "basedpyright-langserver", "--stdio" },
+  filetypes = { "python" },
+  root_markers = {
+    "requirements.txt",
+    ".git",
+  },
+  settings = {
+    basedpyright = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "openFilesOnly",
+        extraPaths = {
+          "./source/isaaclab",
+          "./source/isaaclab_assets",
+          "./source/isaaclab_mimic",
+          "./source/isaaclab_rl",
+          "./source/isaaclab_tasks",
+        },
+      },
+    },
+  },
+}
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,5 +1,15 @@
 return {
   {
-    "echasnovski/mini.nvim",
+    "nvim-mini/mini.nvim",
+  },
+
+  {
+    "nvim-mini/mini.align",
+    opts = {
+      mappings = {
+        start = "<space>fa",
+        start_with_preview = "<space>fA",
+      },
+    },
   },
 }
