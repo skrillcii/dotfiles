2375a5a43d2179049a6fbc2ffd73ed5e57effced
skrillcii <skrillcii@github.com>
2026-01-02 11:09:19 +0900

    update tabby

--- nvim/lua/plugins/tabby.lua
+++ nvim/lua/plugins/tabby.lua
@@ -10,14 +10,25 @@ return {
           lualine_theme = "gruvbox", -- lualine theme name
           tab_name = {
             name_fallback = function(tabid)
-              return tabid
+              local api = require("tabby.module.api")
+              local win_name = require("tabby.feature.win_name")
+              local cur_win = api.get_tab_current_win(tabid)
+              local name = ""
+              if api.is_float_win(cur_win) then
+                name = "[Floating]"
+              else
+                name = win_name.get(cur_win)
+              end
+              return name
             end,
+            override = nil,
           },
           buf_name = {
             mode = "unique", -- or 'relative', 'tail', 'shorten'
           },
         },
       })
+      -- Keybiding
       vim.api.nvim_set_keymap("n", "<leader>th", ":-tabmove<CR>", { noremap = true, desc = "Move tab to previous" })
       vim.api.nvim_set_keymap("n", "<leader>tp", ":+tabmove<CR>", { noremap = true, desc = "Move tab to next" })
       vim.api.nvim_set_keymap(
