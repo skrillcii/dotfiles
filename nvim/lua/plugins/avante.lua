5f37e74d62242a4257eb14e3fea9b3011adb9f76
skrillcii <skrillcii@github.com>
2026-06-01 19:12:45 +0900

    disable avante

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -2,6 +2,7 @@ return {
 
   {
     "yetone/avante.nvim",
+    enabled = false,
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     -- ⚠️ must add this setting! ! !
     build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
